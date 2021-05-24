6dc2700fad251d4039924a4f2cfabc468ad8f0ad
Ogbonnaya Obinna <ogbonnayaobinna@Ogbonnayas-MacBook-Pro.local>
2021-05-24 20:00:31 +0100

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import React, { useState } from 'react';
+import Header from './components/Header'
+import uuid from 'uuidv4'
+import ListItem from './components/ListItem'
+import AddItem from './components/AddItem'
+import {View, Text, StyleSheet, Alert, Image, FlatList} from 'react-native'
+
+const App = () => {
+  const [items, setItems] = useState([
+    {id: uuid(), text: 'Milk'},
+    {id: uuid(), text: 'Eggs'},
+    {id: uuid(), text: 'Bread'},
+    {id: uuid(), text: 'Juice'}
+  ]);
+
+  const deleteItem = (id) => {
+    setItems(prevItems => {
+      return prevItems.filter(item => item.id != id)
+    })
+  }
+
+  const addItem = (item) => {
+    console.log('it is',item)
+    if(!item){
+      console.log('mall')
+    }
+    if(!item) {
+      Alert.alert('Error', 'Text field is null!', [
+        {
+          text: "Ok",
+          onPress: () => console.log("Okay Pressed"),
+          style: "cancel"
+        },
+        //{ text: "OK", onPress: () => console.log("OK Pressed") }
+      ])
+    } else {
+      setItems(prevItems => {
+        return [{id: uuid(), text: item}, ...prevItems];
+      })
+    }
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header title='Shopping List'/>
+      <AddItem addItem={addItem}/>
+      <FlatList 
+        data={items}
+        renderItem={({item}) => <ListItem item={item} deleteItem={deleteItem}/>}
+      />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    paddingTop: 20
+  }
+})
+
+export default App;
